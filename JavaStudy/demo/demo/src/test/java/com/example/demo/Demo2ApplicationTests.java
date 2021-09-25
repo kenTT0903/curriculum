0dacabfea5558e50248a10fc10e5ccc5beb49688
kenTT0903 <69431599+kenTT0903@users.noreply.github.com>
2021-09-25 23:21:50 +0900

    課題提出③ 5章チェックテスト

--- JavaStudy/demo/demo/src/test/java/com/example/demo/Demo2ApplicationTests.java
+++ JavaStudy/demo/demo/src/test/java/com/example/demo/Demo2ApplicationTests.java
@@ -3,7 +3,7 @@ package com.example.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(classes = Demo2ApplicationTests.class)
+@SpringBootTest
 class Demo2ApplicationTests {
 
 	@Test
--- JavaStudy/demo/demo/src/test/java/com/example/demo/DemoApplicationTests.java
+++ JavaStudy/demo/demo/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -3,7 +3,7 @@ package com.example.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(classes = DemoApplicationTests.class)
+@SpringBootTest
 class DemoApplicationTests {
 
 	@Test
