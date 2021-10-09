a5e06bf2bb87827a46a8f471b52b1806b1b44e61
kenTT0903 <69431599+kenTT0903@users.noreply.github.com>
2021-10-09 22:39:09 +0900

    課題提出 1-3

---
+++ javascript/1-3/main.js
@@ -0,0 +1,22 @@
+console.log("a!==10");
+console.log("10<=b<20");
+
+let x = 10;
+if(10<=x<=20)
+{
+    console.log("成功です");
+}
+else
+{
+    conslole.log("失敗です");
+}
+
+let y = 0;
+if(y%2===0)
+{
+    console.log("偶数です")
+}
+else
+{
+    console.log("奇数です")
+}
