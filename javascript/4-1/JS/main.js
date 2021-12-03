872f063c79520e30b15d040ed040785a9c3eb0b4
kenTT0903 <69431599+kenTT0903@users.noreply.github.com>
2021-12-03 13:14:33 +0900

    課題提出 4章チェックテスト②

--- javascript/4-1/JS/main.js
+++ javascript/4-1/JS/main.js
@@ -1,8 +1,10 @@
-var app = new Vue({
+    var app = new Vue({
     el: '#app',
     data: {
         list: [],
         addText: '',
+        serchword:'',
+        serchedlist: [],
         counter: 0,
     },
     //watchでlistの変更を監視
@@ -25,13 +27,25 @@ var app = new Vue({
                 this.list.push({
                     text: this.addText, 
                     isChecked: false,
-                })};
+                });
+            }
             this.addText = '';
         },
         deleteBtn: function() {
             this.list = this.list.filter(function(todo) {
                 return !todo.isChecked;
             });
-        },  
+        },
+        Checked: function(){
+            this.counter = 0
+            for (let i = 0; i < this.list.length; i++) {
+            if (this.list[i].isChecked) {
+            this.counter++;
+            }
+        }
+      },
+        Serch: function(){
+            return todo.text.indexOf(this.serchword);
     }
-}); 
+    }
+    }); 
